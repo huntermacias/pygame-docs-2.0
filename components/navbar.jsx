e2e916315e340a1cf6023ded37669218ab7fc1c7
huntermacias <supercodersf@gmail.com>
2023-03-30 14:04:08 -0700

    updated navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,38 +1,13 @@
 'use client'
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
-
-const BegTopics = [
-	"Color",
-	"Display",
-	"Draw",
-	"Event",
-	"Font",
-	"Image",
-	"Key",
-	"Locals",
-	"Mixer",
-	"Mouse",
-	"Rect",
-	"Surface",
-	"Time",
-	"Music",
-	"Pygame"
-];
-
-const InterTopics = ['cursors', 'joystick', 'mask', 'sprite', 'transform', 'BufferProxy', 'freetype', 'gfxdraw', 'midi', 'PixelArray', 'pixelcopy', 'sndarray', 'surfarray', 'math'];
-
-
-
-  const [dropdownOpenBeg1, setDropdownOpenBeg1] = useState(false);
-  const [dropdownOpenInter, setDropdownOpenInter] = useState(false);
-  const [dropdownOpenAdv, setDropdownOpenAdv] = useState(false);
+	const [dropdownOpenBeg1, setDropdownOpenBeg1] = useState(false);
+	const [dropdownOpenInter, setDropdownOpenInter] = useState(false);
+	const [dropdownOpenAdv, setDropdownOpenAdv] = useState(false);
 
   const handleDropdownToggle = () => {
-	setDropdownOpenInter(false);
-      setDropdownOpenAdv(false);
     setDropdownOpenBeg1(!dropdownOpenBeg1);
   };
 
@@ -48,103 +23,71 @@ const InterTopics = ['cursors', 'joystick', 'mask', 'sprite', 'transform', 'Buff
       setDropdownOpenAdv(!dropdownOpenAdv);
   };
 
+  const BegTopics = ["Color",
+  "Display",
+  "Draw",
+  "Event",
+  "Font",
+  "Image",
+  "Key",
+  "Locals",
+  "Mixer",
+  "Mouse",
+  "Rect",
+  "Surface",
+  "Time",
+  "Music",
+  "Pygame"];
+
+  const InterTopics = ['cursors', 'joystick', 'mask', 'sprite', 'transform', 'BufferProxy', 'freetype', 'gfxdraw', 'midi', 'PixelArray', 'pixelcopy', 'sndarray', 'surfarray', 'math'];
+
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-				<Link href="/">
-              		<img className="h-8 w-32" src="https://www.pygame.org/docs/_images/pygame_lofi.png" alt="pygame logo" />
-
-				</Link>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                {/* <Link href="/">
-                  <span className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</span>
+    <nav className="flex justify-center bg-gray-800">
+      <div className="relative">
+        <button
+          className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-mono focus:outline-none"
+          onClick={handleDropdownToggle}
+        >
+          Beginner Topics
+        </button>
+        {dropdownOpenBeg1 && (
+          <div
+            className="absolute z-10 top-full left-1/2 w-1/2 transform -translate-x-1/2 bg-gray-800 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden"
+			style={{ width: '300px', maxHeight: '200px' }}
+          >
+            <div className="grid grid-cols-3 gap-4 px-5 py-6 sm:px-8 sm:py-8">
+              {BegTopics.map((topic) => (
+                <Link key={topic} href={`/${topic.toLowerCase().replace(/\s+/g, '-')}`}>
+                  <span className="block font-medium text-emerald-400 hover:text-emerald-300">{topic}</span>
                 </Link>
-                <Link href="/about">
-                  <span className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</span>
-                </Link>
-                
-				 */}
-				<div className="relative">
-					<button className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none" onClick={handleDropdownToggle}>
-						Beginner Topics
-					</button>
-					{dropdownOpenBeg1 && (
-						<div className="absolute z-10 -ml-4 mt-3 transform -translate-x-1/2 md:-translate-x-0 px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2 lg:mt-1 bg-gray-800/10">
-						<div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-							<div className="relative grid grid-cols-3 gap-6 bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
-							{BegTopics.map((topic) => (
-							<Link key={topic} href={`/${topic.toLowerCase().replace(/\s+/g, '-')}`}>
-								<span className="block font-medium text-emerald-400 hover:text-emerald-300">
-								{topic}
-								</span>
-							</Link>
-							))}
-
-							</div>
-						</div>
-						</div>
-					)}
-				</div>
-
-
-
-				<div className="relative">
-					<button className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none" onClick={handleDropdownToggleInter}>
-						Intermediate Topics
-					</button>
-					{dropdownOpenInter && (
-						<div className="absolute z-10 -ml-4 mt-3 transform -translate-x-1/2 md:-translate-x-0 px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/4 lg:-translate-x-1/2 lg:mt-1 bg-gray-800/10">
-						<div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-							<div className="relative grid grid-cols-3 gap-6 bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
-							{InterTopics.map((topic) => (
-							<Link key={topic} href={`/${topic.toLowerCase().replace(/\s+/g, '-')}`}>
-								<span className="block font-medium text-emerald-400 hover:text-emerald-300">
-								{topic}
-								</span>
-							</Link>
-							))}
-
-							</div>
-						</div>
-						</div>
-					)}
-				</div>
-
-					<div className="relative">
-					<button
-						className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none"
-						onClick={handleDropdownToggleAdv}
-					>
-						Advances Topics
-					</button>
-					{dropdownOpenAdv && (
-						<div
-						className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2 lg:mt-1 bg-gray-800/10"
-						>
-						<div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-							<div className="relative grid gap-6 bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
-							<Link href="/">
-								<span className="block font-medium text-emerald-400 hover:text-emerald-300">
-								Topic 1
-								</span>
-							</Link>
-							</div>
-						</div>
-						</div>
-					)}
-					</div>
-
-
-
-              </div>
+              ))}
             </div>
           </div>
-        </div>
+        )}
+      </div>
+
+
+	  <div className="relative">
+        <button
+          className="text-emerald-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-mono focus:outline-none"
+          onClick={handleDropdownToggleInter}
+        >
+          Intermediate Topics
+        </button>
+        {dropdownOpenInter && (
+          <div
+            className="absolute z-10 top-full left-1/2 w-1/2 transform -translate-x-1/2 bg-gray-800 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden"
+			style={{ width: '330px', maxHeight: '200px' }}
+          >
+            <div className="grid grid-cols-3 gap-4 px-5 py-6 sm:px-8 sm:py-8">
+              {InterTopics.map((topic) => (
+                <Link key={topic} href={`/${topic.toLowerCase().replace(/\s+/g, '-')}`}>
+                  <span className="block font-medium text-emerald-400 hover:text-emerald-300">{topic}</span>
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
