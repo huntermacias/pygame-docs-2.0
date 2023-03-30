e6bddccdfd6f2710d820502291109c730bef5e7d
huntermacias <supercodersf@gmail.com>
2023-03-29 23:01:46 -0700

    removed cn function

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -2,29 +2,26 @@
 import * as React from 'react'
 import * as TabsPrimitive from '@radix-ui/react-tabs'
 
-import { cn } from '../lib/utils'
+// import { cn } from '../lib/utils'
 
 const Tabs = TabsPrimitive.Root
+
 
 const TabsList = (({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
-    className={cn(
-      'inline-flex items-center justify-center rounded-md bg-slate-100 p-1 dark:bg-slate-800',
-      className
-    )}
-    {...props}
+    className={
+      'inline-flex items-center justify-center rounded-md bg-slate-100 p-1 dark:bg-slate-800'
+    }
   />
 ))
 TabsList.displayName = TabsPrimitive.List.displayName
 
 const TabsTrigger = (({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
-    className={cn(
-      'inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-3 py-1.5  text-sm font-medium text-slate-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm dark:text-slate-200 dark:data-[state=active]:bg-slate-900 dark:data-[state=active]:text-slate-100',
-      className
-    )}
-    {...props}
+    className={
+      'inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-3 py-1.5  text-sm font-medium text-slate-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm dark:text-slate-200 dark:data-[state=active]:bg-slate-900 dark:data-[state=active]:text-slate-100'
+    }
     ref={ref}
   />
 ))
@@ -32,11 +29,8 @@ TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
 const TabsContent = (({ className, ...props }, ref) => (
   <TabsPrimitive.Content
-    className={cn(
-      'mt-2 rounded-md border border-slate-300 bg-slate-100 p-6 dark:border-slate-700 dark:bg-slate-900 w-full',
-      className
-    )}
-    {...props}
+    className={
+      'mt-2 rounded-md border border-slate-300 bg-slate-100 p-6 dark:border-slate-700 dark:bg-slate-900 w-full'}
     ref={ref}
   />
 ))
