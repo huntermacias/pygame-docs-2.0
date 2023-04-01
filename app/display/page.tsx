de60498e19bbc93b4cdeacc19ea69ad5853ea2a0
Edward Rees <erees.hk@gmail.com>
2023-03-31 19:06:23 -0700

    Added more methods

--- app/display/page.tsx
+++ app/display/page.tsx
@@ -10,11 +10,52 @@ const Display = () => {
       description: 'Initializes the display module',
       key: '1',
       codesample: 'display.init()'
+    },
+    {
+      title: ' display.quit() - None',
+      description: 'Uninitializes the display module',
+      key: '2',
+      codesample: 'display.quit()'
+    },
+    {
+      title: ' display.get_init() - bool',
+      description: 'Returns True if the display module has been initialized',
+      key: '3',
+      codesample: 'display.get_init()'
+    },
+    {
+      title: ' display.set_mode(size, flags=0, depth=0) - Surface',
+      description: 'Creates a display Surface and returns it',
+      key: '4',
+      codesample: 'display.set_mode((800, 600))'
+    },
+    {
+      title: ' display.get_surface() - Surface',
+      description: 'Returns the display Surface',
+      key: '5',
+      codesample: 'display.get_surface()'
+    },
+    {
+      title: ' display.flip() - None',
+      description: 'Updates portions of the screen',
+      key: '6',
+      codesample: 'display.flip()'
+    },
+    {
+      title: ' display.update(rectangle=None) - None',
+      description: 'Updates portions of the screen for software displays',
+      key: '7',
+      codesample: 'display.update()'
+    },
+    {
+      title: ' display.Info() - VideoInfo',
+      description: 'Create a video display information object', // TODO - add more info
+      key: '8',
+      codesample: 'display.Info()'
     }
-
   ];
   
-  const colordescription = [
+  const description = [
     'pygame.display is a module that allows you to control the display window and screen'
   ]
 
@@ -62,7 +103,7 @@ const Display = () => {
           </ul>
         </div>
         <div className="mt-10 p-4 pb-40">
-          <TextBox title="PyGame Color Description" description={colordescription[0]} />
+          <TextBox title="PyGame Color Description" description={description[0]} />
          
         </div>
         
