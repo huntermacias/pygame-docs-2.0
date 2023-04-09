28cd25b0ae4cd858a889453a9c9c774ae673382b
huntermacias <supercodersf@gmail.com>
2023-04-08 19:03:00 -0700

    improved homepage resizing

--- app/page.tsx
+++ app/page.tsx
@@ -103,8 +103,8 @@ export default function Home() {
       <main className="max-w-7xl mx-auto px-4 lg:px-8 py-12 flex flex-col md:flex-row md:space-x-8">
         <div className="w-full md:w-4/5">
           
-          <h1 className="text-5xl font-bold mb-4">Welcome to Pygame Docs</h1>
-          <p className="text-xl mb-8">
+          <h1 className="text-2xl md:text-5xl font-bold mb-4 ">Welcome to Pygame Docs</h1>
+          <p className="text-sm md:text-xl mb-8">
             Pygame is a set of Python modules designed for writing video games.
             Pygame adds functionality on top of the SDL library. This allows you to
             create fully featured games and multimedia programs in the python
@@ -117,19 +117,19 @@ export default function Home() {
 
           <div className="bg-gray-900 text-white py-12 mt-8 max-w-4xl mx-auto">
             <div className="px-4 sm:px-6 lg:px-8">
-              <h2 className="text-3xl font-bold mb-6">Get started with PyGame</h2>
-              <p className="text-lg mb-8">
+              <h2 className="text-xl md:text-3xl font-bold mb-6">Get started with PyGame</h2>
+              <p className="text-sm md:text-lg mb-8">
                 Follow these simple steps to start creating games with PyGame:
               </p>
               <ol className="list-decimal list-inside mb-8">
-                <li className="mb-2">Install PyGame using pip:</li>
+                <li className="mb-2 text-sm md:text-lg">Install PyGame using pip:</li>
                 <code className="block bg-gray-800 rounded-md p-4 mb-4">
                   $ pip install pygame
                 </code>
               </ol>
               <GettingStarted code={gettingstart} />
               <hr />
-              <p className="text-lg">
+              <p className="text-sm md:text-lg pt-8">
                 Pygame is a powerful library for creating games and interactive
                 applications in Python. With Pygame, you can create anything from
                 simple 2D games to complex simulations and visualizations. Get started
