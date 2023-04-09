8c376c6db12f3ab57f2f4993c3e49b6d40f6d1bb
Edward Rees <erees.hk@gmail.com>
2023-04-09 10:43:19 -0700

    Changed `a` in Navbar to `Link`

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <a href="/">
+            <Link href="/">
               <Image
                 className="block h-10 w-auto"
                 src="https://cdn-icons-png.flaticon.com/512/2570/2570575.png"
@@ -38,7 +38,7 @@ const Navbar = () => {
                 width={50}
                 height={50}
               />
-            </a>
+            </Link>
           </div>
           <div className="flex items-center lg:hidden">
             <button
@@ -56,12 +56,12 @@ const Navbar = () => {
             </button>
           </div>
           <div className="hidden lg:flex lg:items-center lg:justify-end lg:flex-1 font-mono">
-            <a
+            <Link
               href="/"
               className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-white"
             >
               Home
-            </a>
+            </Link>
             <div className="relative ml-8">
               <button
                 onClick={() => setIsOpen(!isOpen)}
@@ -80,6 +80,7 @@ const Navbar = () => {
                       <Link
                         key={topic}
                         href={`/${topic.toLowerCase().replace(/\s+/g, "-")}`}
+                        onClick={() => setIsOpen(!isOpen)}
                       >
                         <span className="block px-4 py-2 text-sm text-white bg-gray-900 hover:bg-gray-800">
                           {topic}
@@ -92,18 +93,18 @@ const Navbar = () => {
             </div>
            
 
-            <a
+            <Link
               href="/donate"
               className="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-white"
             >
               Donate
-            </a>
-            <a
+            </Link>
+            <Link
               href="/"
               className="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-white"
             >
               Log in
-            </a>
+            </Link>
             <Link
               href="/signup"
               className="ml-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
@@ -116,18 +117,18 @@ const Navbar = () => {
 
       <div className={isOpen ? "block lg:hidden" : "hidden"} id="mobile-menu">
         <div className="pt-2 pb-3 space-y-1">
-          <a
+          <Link
             href="/"
             className="block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-gray-500 hover:text-gray-900 hover:bg-gray-50"
           >
             Home
-          </a>
-          <a
+          </Link>
+          <Link
             href="/"
             className="block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-gray-500 hover:text-gray-900 hover:bg-gray-50"
           >
             Features
-          </a>
+          </Link>
 
           <Link
             href="/donate"
@@ -136,12 +137,12 @@ const Navbar = () => {
             Donate
           </Link>
 
-          <a
+          <Link
             href="/"
             className="block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-gray-500 hover:text-gray-900 hover:bg-gray-50"
           >
             Log in
-          </a>
+          </Link>
         </div>
         <Link
           href="/signup"
