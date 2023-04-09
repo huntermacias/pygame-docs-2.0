a62714f17f43e1f2f5bfd69901eb786483c76b94
huntermacias <supercodersf@gmail.com>
2023-04-08 18:32:47 -0700

    ui improved for modal

--- components/PygameModal.jsx
+++ components/PygameModal.jsx
@@ -81,7 +81,8 @@ const PygameModal = () => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-auto text-left align-middle transition-all transform bg-gray-800 shadow-xl rounded-2xl">
+             <div className="inline-block w-full max-w-md p-6 my-8 overflow-auto text-left align-middle transition-all transform bg-gray-800 shadow-xl rounded-2xl max-w-4xl" style={{maxWidth: '80rem'}}>
+
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-100"
