4a5a6939c1522c922bd833007357828ad89553ec
huntermacias <supercodersf@gmail.com>
2023-04-10 12:53:33 -0700

    refactored key page

--- app/key/page.tsx
+++ app/key/page.tsx
@@ -1,27 +1,17 @@
 'use client'
-import { Fragment } from 'react';
-import GettingStarted from '../../components/GettingStarted';
-import TextBox from '../../components/TextBox';
+
 import { data, description } from "./docs"
+import DocLayout from '../../components/DocLayout';
 
 const KeyPage = () => {
 
   return (
-    <div className="mx-auto max-w-2xl p-6 mb-10">
-      <h1 className="text-2xl font-extralight font-mono mb-6 text-amber-300">{description.title}</h1>
-      {data.map((data) => (
-        <Fragment key={data.title}>
-          <div className='text-amber-200 font-mono text-sm pb-4'>
-
-            <TextBox title={data.title} description={data.description} />
-            <GettingStarted code={data.codesample} />
-          </div>
-        </Fragment>
-      ))}
-
-	  <div></div>
-	
-    </div>
+    <DocLayout 
+      title={description.title} 
+      baseDescription={description.description} 
+      data={data} 
+      description={description} 
+    />
 
   );
 };
