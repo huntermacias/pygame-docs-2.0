018d0b121ef3fc4ee26fa43fe28cf33709ceed33
huntermacias <supercodersf@gmail.com>
2023-04-10 12:51:10 -0700

    refactored display page

--- app/display/page.tsx
+++ app/display/page.tsx
@@ -1,30 +1,19 @@
 'use client'
-import { Fragment } from 'react';
-import GettingStarted from '../../components/GettingStarted';
-import TextBox from '../../components/TextBox';
-import { data, description } from "./docs"
 
-const FontPage = () => {
+import { data, description } from "./docs"
+import DocLayout from '../../components/DocLayout';
+
+const DisplayPage = () => {
 
   return (
-    <div className="mx-auto max-w-2xl p-6 mb-10">
-      <h1 className="text-3xl font-bold font-mono mb-6 text-amber-300">Examples for {description.title}</h1>
-      {data.map((data) => (
-        <Fragment key={data.title}>
-          <h2 className="text-xl font-bold font-mono mb-2 text-amber-300">{data.description}</h2>
-          <div className='text-white text-sm pb-4'>
-
-            <TextBox title={data.title} description={data.description} />
-            <GettingStarted code={data.codesample} />
-          </div>
-        </Fragment>
-      ))}
-
-	  <div></div>
-	
-    </div>
+    <DocLayout 
+      title={description.title} 
+      baseDescription={description.description} 
+      data={data} 
+      description={description} 
+    />
 
   );
 };
 
-export default FontPage;
+export default DisplayPage;
