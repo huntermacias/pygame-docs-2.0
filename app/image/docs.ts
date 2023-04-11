ea6776251fde8301bb97e37589e8baa5643d1cf5
huntermacias <supercodersf@gmail.com>
2023-04-10 23:44:44 -0700

    refactored mixer and image page

---
+++ app/image/docs.ts
@@ -0,0 +1,67 @@
+export type Info = {
+	title: string;
+	description: string;
+	key: string;
+	codesample: string;
+  };
+  const data: Info[] = [
+	{
+      title: 'pygame.image.load()',
+      codesample: `cloudImage = pygame.image.load("myCloud.png").convert_alpha()
+cloudImage2 = pygame.image.load("myCloud2.jpg").convert()`,
+	  key: "1",
+	  description: `To load an image, use the pygame.image.load() function. This function accepts a file path or file-like object and returns a Surface object. The following image formats are supported:
+BMP
+GIF (non-animated)
+JPEG
+LBM (and PBM, PGM, PPM)
+PCX
+PNG
+PNM
+SVG (limited support, using Nano SVG)
+TGA (uncompressed)
+TIFF
+WEBP
+XPM
+New in Pygame 2.0: Loading SVG, WebP, PNM 
+	  `,
+    },
+	{
+		title: 'pygame.image.save()',
+		codesample: `import pygame
+
+# init setup
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+running = True
+clock = pygame.time.Clock()
+
+
+while running:
+	# set background black
+	screen.fill((0,0,0))
+
+	# event loop
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_s:
+				# save image
+				pygame.image.save(screen, "image.png")
+		draw(event)
+	pygame.display.flip()
+pygame.quit()`,
+		key: '2',
+		description: 'This will save your Surface as either a BMP, TGA, PNG, or JPEG image. If the filename extension is unrecognized it will default to TGA. Both TGA, and BMP file formats create uncompressed files. You can pass a filename, a pathlib.Path or a Python file-like object. For file-like object, the image is saved to TGA format unless a namehint with a recognizable extension is passed in.'
+	  },
+  ];
+
+  const description = {
+	title: "pygame.image Module",
+	description:
+	  "Welcome to the Pygame Image module! This module is designed to provide you with powerful tools for loading and manipulating images in your games and interactive applications.",
+  };
+  
+  export { data, description };
+  
--- app/mixer/docs.ts
+++ app/mixer/docs.ts
@@ -6,62 +6,53 @@ export type Info = {
   };
   const data: Info[] = [
 	{
-	  title: " pygame.key.get_pressed() - bools",
-	  description: "Returns a sequence of boolean values representing the state of every key on the keyboard. Use the key constant values to index the array. A True value means that the button is pressed.",
+	  title: " pygame.mixer.init()",
+	  description: "Initialize the mixer module for Sound loading and playback",
 	  key: "1",
-	  codesample: `speed = [5, 10] # speed for x & y direction
-key = pygame.key.get_pressed() # list of bools for each each key
+	  codesample: `import pygame
 
-# decrease x speed when the 'a' key is pressed
-if (key[pygame.K_a]): 
-	speed[0] -= 0.2
-# increase x speed when the 'd' key is pressed
-if (key[pygame.K_d]): 
-	speed[0] += 0.2
-# increase y speed when the 'w' key is pressed
-if (key[pygame.K_w]): 
-	speed[1] += 0.2
-# decrease the y speed when the 's' key is pressed
-if (key[pygame.K_s]): 
-	speed[1] -= 0.2
+pygame.mixer.init()
 `,
 	},
 	{
-	  title: " pygame.key.get_focused() - bool",
-	  description: "Returns True when the display window has keyboard focus from the system. If the display needs to ensure it does not lose keyboard focus, it can use pygame.event.set_grab() to control the sharing of input devices with other applications to grab all input.",
+	  title: " pygame.mixer.Sound()",
+	  description: "pygame.mixer.Sound() -> Sound : Create a new Sound object from a file or buffer object",
 	  key: "2",
 	  codesample: `import pygame
 
 pygame.init()
 
-# main loop
-while True:
+track = pygame.mixer.Sound("song.mp3")
 
-	# event loop
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
+# set volume of track - (0-1)
+track.set_volume(0.6)
 
-		# check if any key is down
-		if event.type == pygame.KEYDOWN:
-			# check if display is receiving keyboard input 
-			if pygame.key.get_focused() and pygame.key.get_pressed()[K_SPACE]:
-				print(pygame.key.get_focused())
-				is_paused = not is_paused
-				`,
+# get volume level
+currentVolume = track.get_volume()
+
+# start playing the track
+track.play()
+
+
+`,
 	},
 	{
-		title: " pygame.key.key_code() - int",
-		description: "Get the key identifier code from the descriptive name of the key. This returns an integer matching one of the K_* keycodes. For example: ",
+		title: " pygame.mixer.Sound.play()",
+		description: "Begin playback of the Sound (i.e., on the computer's speakers) on an available Channel. This will forcibly select a Channel, so playback may cut off a currently playing sound if necessary.",
 		key: "3",
-		codesample: `>>> pygame.key.key_code("return") == pygame.K_RETURN
-True
+		codesample: `import pygame
 
->>> pygame.key.key_code("0") == pygame.K_0
-True
+pygame.init()
 
->>> pygame.key.key_code("space") == pygame.K_SPACE
-True
+# load track
+track = pygame.mixer.Sound("song.mp3")
+
+# start playing the track
+track.play()
+
+# 
+
+
   `,
 	  },
   ];
