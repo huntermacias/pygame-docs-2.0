c9b1acff4607e97cb6b9b67b0f8f5658d3753059
huntermacias <supercodersf@gmail.com>
2023-04-13 23:15:53 -0700

    updated learning page

--- documentation/learning_docs.ts
+++ documentation/learning_docs.ts
@@ -6,77 +6,48 @@ export type LearnBegInfo = {
   };
   
 const pygameSteps: LearnBegInfo[] = [
-{
-	title: "Install Pygame",
-	description:
-	"First, you need to install Pygame. You can do this using pip, which is a package manager for Python. Open your terminal and run the following command:",
-	key: "1",
-	codesample: `>> pip install pygame`,
-},
-{
-	title: "Import Pygame",
-	description:
-	"Once you have installed Pygame, you need to import it in your Python file:",
-	key: "2",
-	codesample: `import pygame
-	`,
-},
-{
-	title: "Initialize Pygame",
-	description:
-	"Before you can use Pygame, you need to initialize it. This is done using the `pygame.init()` method:",
-	key: "3",
-	codesample: `import pygame
-
-pygame.init()
-	`,
-},
-{
-	title: "Create a Window",
-	description:
-	"Now that Pygame is initialized, you can create a window to display your game:",
-	key: "4",
-	codesample: `import pygame
-
-pygame.init()
-
-width = 800
-height = 600
-
-screen = pygame.display.set_mode((width, height))
-	`,
-},
-{
-	title: "The Main Game Loop",
-	description:
-	"Finally, you need to create a main game loop. This loop will run continuously and handle user input, update the game state, and draw the game to the screen:",
-	key: "5",
-	codesample: `
-	import pygame
-
-	pygame.init()
-
-	width = 800
-	height = 600
-
-	screen = pygame.display.set_mode((width, height))
-
-	running = True
-
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-
-		# Update the game state
-
-		# Draw the game to the screen
-
-		pygame.display.update()
-
-	pygame.quit()
-	`,
-},
+	{
+		title: "Install Pygame",
+		description:
+		  "First, you need to install Pygame. You can do this using pip, which is a package manager for Python. Open your terminal and run the following command:",
+		key: "1",
+		codesample: ">> pip install pygame",
+	  },
+	  {
+		title: "Import Pygame",
+		description:
+		  "Once you have installed Pygame, you need to import it in your Python file:",
+		key: "2",
+		codesample: "import pygame",
+	  },
+	  {
+		title: "Initialize Pygame",
+		description:
+		  "Before you can use Pygame, you need to initialize it. This is done using the `pygame.init()` method:",
+		key: "3",
+		codesample: `import pygame\n\npygame.init()`,
+	  },
+	  {
+		title: "Create a Window",
+		description:
+		  "Now that Pygame is initialized, you can create a window to display your game:",
+		key: "4",
+		codesample: `import pygame\n\npygame.init()\n\nwidth = 800\nheight = 600\n\nscreen = pygame.display.set_mode((width, height))`,
+	  },
+	  {
+		title: "The Main Game Loop",
+		description:
+		  "Finally, you need to create a main game loop. This loop will run continuously and handle user input, update the game state, and draw the game to the screen:",
+		key: "5",
+		codesample: `import pygame\n\npygame.init()\n\nwidth = 800\nheight = 600\n\nscreen = pygame.display.set_mode((width, height))\n\nrunning = True\n\nwhile running:\n    for event in pygame.event.get():\n        if event.type == pygame.QUIT:\n            running = False\n\n    # Update the game state\n\n    # Draw the game to the screen\n\n    pygame.display.update()\n\npygame.quit()`,
+	  },
+	  {
+		title: "Load and Display an Image",
+		description:
+		  "You can load an image and display it on the screen using the `pygame.image.load()` and `blit()` methods:",
+		key: "6",
+		codesample: `import pygame\n\npygame.init()\n\nwidth = 800\nheight = 600\n\nscreen = pygame.display.set_mode((width, height))\n\nimage = pygame.image.load("image.png")\n\nwhile True:\n    for event in pygame.event.get():\n        if event.type == pygame.QUIT:\n            pygame.quit()\n            sys.exit()\n\n    screen.blit(image, (0, 0))\n\n    pygame.display.update()`,
+	  },
 ];
 
 const pygameIntroduction = {
--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -1,8 +1,10 @@
-'use client'
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const PygameCards = () => {
+  const [activeCard, setActiveCard] = useState(true);
+
   const cards = [
     {
       title: "Pygame Basics",
@@ -10,6 +12,11 @@ const PygameCards = () => {
         "Get started on your journey of creating fun and exciting games with Pygame! In this beginner-friendly course, you'll learn the fundamentals of Pygame, from setting up your development environment to creating your first game. With easy-to-follow tutorials and hands-on exercises, you'll be able to bring your imagination to life and start creating games that you can be proud of. Join now and unlock the power of Pygame!",
       imageUrl: "https://cdn-icons-png.flaticon.com/512/2621/2621053.png",
       linkUrl: "/pygame-basics",
+      options: [
+        { label: "Pygame Basics Details", link: "/pygame-basics" },
+        { label: "View on GitHub", link: "https://github.com/username/repo" },
+        { label: "View Demo", link: "https://demo.com" },
+      ],
     },
     {
       title: "Pygame Advanced",
@@ -17,31 +24,98 @@ const PygameCards = () => {
         "Discover the limitless possibilities of Pygame and explore advanced concepts and techniques to create games that will impress your friends and players alike. This course will challenge you to push your Pygame skills to their limits and create immersive gaming experiences that will leave a lasting impression. Take the next step in your Pygame journey and become a master game developer.",
       imageUrl: "https://cdn-icons-png.flaticon.com/512/2621/2621053.png",
       linkUrl: "/pygame-advanced",
+      options: [
+        {
+          label: "Pygame Advanced Details",
+          link: "/pygame-advanced",
+        },
+        {
+          label: "View on GitHub",
+          link: "https://github.com/username/repo",
+        },
+        {
+          label: "View Demo",
+          link: "https://demo.com",
+        },
+      ],
     },
-	{
-		title: "Full Game Examples",
-		description: "Unlock the full potential of Pygame and unleash your creativity to create immersive gaming experiences. This course will take you on a deep dive into advanced Pygame concepts and techniques, providing you with the skills and knowledge necessary to develop games that will leave a lasting impression. Whether you're a seasoned game developer or just starting out, this course will challenge your pygame knowledge.",
-		imageUrl: "https://cdn-icons-png.flaticon.com/512/2621/2621053.png",
-		linkUrl: "/pygame-examples-learn",
-	}
+    {
+      title: "Full Game Examples",
+      description:
+        "Unlock the full potential of Pygame and unleash your creativity to create immersive gaming experiences. This course will take you on a deep dive into advanced Pygame concepts and techniques, providing you with the skills and knowledge necessary to develop games that will leave a lasting impression. Whether you're a seasoned game developer or just starting out, this course will challenge your pygame knowledge.",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/2621/2621053.png",
+      linkUrl: "/pygame-examples-learn",
+      options: [
+        {
+          label: "Full Game Examples Details",
+          link: "/pygame-examples-learn",
+        },
+        {
+          label: "View on GitHub",
+          link: "https://github.com/username/repo",
+        },
+        {
+          label: "View Demo",
+          link: "https://demo.com",
+        },
+      ],
+    },
   ];
 
+  const handleCardClick = (index) => {
+    if (activeCard === index) {
+      setActiveCard(false);
+    } else {
+      setActiveCard(index);
+    }
+  };
+
   return (
-    <div className="py-10">
+    <div className="py-10 font-mono">
       <div className="container mx-auto">
         <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {cards.map((card, index) => (
-            <div key={index} className="overflow-hidden shadow-lg rounded-lg bg-gray-800 ">
-              <Image className="pl-6 pt-3" alt={card.title} src={card.imageUrl} width={200} height={200} />
+            <div
+              key={index}
+              className="overflow-hidden shadow-lg rounded-lg bg-gray-800 "
+            >
+              <Image
+                className="pl-6 pt-3"
+                alt={card.title}
+                src={card.imageUrl}
+                width={200}
+                height={200}
+              />
               <div className="px-6 py-4 mb-2">
-                <h2 className="font-mono font-bold text-xl mb-2 text-amber-400">{card.title}</h2>
-                <p className="text-gray-300 text-base mb-4 space-y-2">{card.description}</p>
-                <Link href={card.linkUrl}>
-                  <span className="bottom w-1/4 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 
-				    rounded-lg transition-colors duration-300">
-                    Learn More
-                  </span>
-                </Link>
+                <h2 className="font-mono font-bold text-xl mb-2 text-amber-400">
+                  {card.title}
+                </h2>
+                <p className="text-gray-300 text-base mb-4 space-y-2">
+                  {card.description}
+                </p>
+                <div className="relative">
+                  <button
+                    onClick={() => handleCardClick(index)}
+                    className="bottom w-1/2 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
+                  >
+                    Get Started
+                  </button>
+
+                  {activeCard && (
+                    <ul className="relative mt-4 text-xl bg-gray-900 rounded-md shadow-lg py-2">
+                      {cards.map((card, index) => (
+                        <div  key={index}>
+                          <li className="px-4 py-2 rounded-md hover:bg-gray-800 text-white">
+                            <a href={card.linkUrl}>{card.title}</a>
+                          </li>
+
+                        </div>
+
+                      ))}
+                    
+                    </ul>
+                  )}
+                </div>
               </div>
             </div>
           ))}
