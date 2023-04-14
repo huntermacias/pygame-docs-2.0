8c88e1e4bc60bd860805db1503a4347748767d82
huntermacias <supercodersf@gmail.com>
2023-04-14 12:34:39 -0700

    layout build test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
-import dynamic from 'next/dynamic';
-import type { AppProps } from 'next/app';
-import './globals.css';
+import './globals.css'
+import type { AppProps } from 'next/app'
+import RootLayout from './layout'
 
-const RootLayout = dynamic(() => import('./../components/layout'), {
-  ssr: false,
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
-  );
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    ) 
+    
 }
 
-export default MyApp;
+export default MyApp
