7ce531216bd6c42fd5f9562e345dcb7ced107ab5
huntermacias <supercodersf@gmail.com>
2023-04-14 22:46:38 -0700

    updated learning ui

--- components/LearningCard.tsx
+++ components/LearningCard.tsx
@@ -22,7 +22,7 @@ export default function LearningCard({ cards }: Props) {
       {cards.map((card, index) => (
         <div
           key={index}
-          className="overflow-hidden shadow-lg rounded-lg bg-gray-800 "
+          className="overflow-hidden shadow-lg rounded-lg bg-[#121212]"
         >
           <Image
             className="pl-6 pt-3"
@@ -32,21 +32,21 @@ export default function LearningCard({ cards }: Props) {
             height={200}
           />
           <div className="px-6 py-4 mb-2">
-            <h2 className="font-mono font-bold text-xl mb-2 text-amber-400">
+            <h2 className="font-mono font-extralight underline underline-offset-4 text-xl md:text-3xl mb-2 text-white">
               {card.title}
             </h2>
-            <p className="text-gray-300 text-base mb-4 space-y-2">
+            <p className="text-gray-300 mb-4 space-y-2">
               {card.description}
             </p>
             <div className="relative">
               <button
                 onClick={() => handleCardClick(index)}
-                className="bottom w-1/2 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
+                className="bottom w-1/2 bg-blue-600 hover:bg-[#2b2b2b] shadow-lg shadow-bg-[#0a0a0a] text-white font-normal py-2 px-4 rounded-lg transition-colors duration-200 hover:font-bold"
               >
                 Get Started
               </button>
 
-              <ul className="relative mt-4 text-lg bg-gray-900 rounded-md shadow-lg py-2">
+              <ul className="relative mt-4 text-md md:text-xl bg-[#0a0a0a] rounded-md shadow-lg py-2">
                 <div>
                   {card.options.map((option, key) => (
                     <div key={key}>
@@ -54,7 +54,7 @@ export default function LearningCard({ cards }: Props) {
                         <div key={index}>
                           <div
                             key={key}
-                            className="px-4 py-2 rounded-md hover:bg-gray-800 hover:w-1/2 hover:ml-2 text-white"
+                            className="px-4 py-2 rounded-md xl:hover:w-3/4 hover:bg-[#2b2b2b] hover:ml-2 text-white font-extralight"
                           >
                             <a href={option.link}>{option.label}</a>
                           </div>
--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -9,7 +9,7 @@ const PygameCards = () => {
       <div className="container mx-auto">
         <div className="relative grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {card.map((card, index) => (
-            <div key={index} className="overflow-hidden mb-14 shadow-lg rounded-lg bg-gray-800 ">
+            <div key={index} className="overflow-hidden mb-14 shadow-lg rounded-lg bg-[#0a0a0a] ">
               <LearningCard cards={[card]} />
             </div>
           ))}
