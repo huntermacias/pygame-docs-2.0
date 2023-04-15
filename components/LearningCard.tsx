40d1155444bcc89af1a8a253051ace8fad52ba20
huntermacias <supercodersf@gmail.com>
2023-04-15 14:39:33 -0700

    working on button dropdown

--- components/LearningCard.tsx
+++ components/LearningCard.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
-import React from "react";
-import { Card } from "./../types";
-
+import React, { useState } from "react";
+import { Card } from "../types";
 
 interface Props {
   card: Card;
 }
 
-export default function LearningCard({ card }: Props) {
+const LearningCard: React.FC<Props> = ({ card }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="overflow-hidden rounded-lg">
       <Image
@@ -22,28 +27,29 @@ export default function LearningCard({ card }: Props) {
           {card.title}
         </h2>
         <p className="text-gray-300 mb-4 px-2">{card.description}</p>
-        <div className="relative">
-          <button className="bottom w-1/2 bg-blue-800 hover:bg-[#2b2b2b] text-white font-normal py-2 px-4 rounded-lg transition-colors duration-200 hover:font-bold">
+        <div className="relative z-index">
+          <button 
+            onClick={() => {!isOpen}}
+            className="bottom w-1/2 bg-blue-800 hover:bg-[#2b2b2b] text-white font-normal py-2 px-4 rounded-lg transition-colors duration-200 hover:font-bold">
             Get Started
           </button>
-          <div>
-            <ul className="relative mt-4 p-2 text-md md:text-xl bg-[#0a0a0a] rounded-md py-2">
-              {card.options.map((option, key) => (
-                <li key={key}>
-                 <div key={key}>
-                    <div
-                      key={key}
-                      className="px-4 py-2 rounded-md xl:hover:w-3/4 hover:bg-[#2b2b2b] text-sm md:text-lg hover:ml-2 text-white font-extralight"
-                    >
-                      <a href={option.link}>{key+1}. {option.label}</a>
+          {isOpen && (
+            <div>
+              <ul className="relative mt-4 p-2 text-md md:text-xl bg-[#0a0a0a] rounded-md py-2">
+                {card.options.map((option, key) => (
+                  <li key={key}>
+                    <div className="px-4 py-2 rounded-md xl:hover:w-3/4 hover:bg-[#2b2b2b] text-sm md:text-lg hover:ml-2 text-white font-extralight">
+                      <a href={option.link}>{key + 1}. {option.label}</a>
                     </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default LearningCard;
