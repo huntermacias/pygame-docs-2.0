4a10d26851f4daf53436413b5068cd191ce6a0ed
huntermacias <supercodersf@gmail.com>
2023-04-15 21:43:03 -0700

    testing supabase data on display docs page

--- documentation/docs.ts
+++ documentation/docs.ts
@@ -86,80 +86,6 @@ const colorDescription = {
   };
 
 
-
-export type displayInfo = {
-title: string;
-description: string;
-shortDesc: string;
-key: string;
-codesample: string;
-  };
-const displayData: displayInfo[] = [
-{
-  title: " display.init() - None",
-  description: "Initializes the display module",
-  shortDesc: "",
-  key: "1",
-  codesample: "display.init()",
-},
-{
-  title: " display.quit() - None",
-  description: "Uninitializes the display module",
-  shortDesc: "",
-  key: "2",
-  codesample: "display.quit()",
-},
-{
-  title: " display.get_init() - bool",
-  description: "Returns True if the display module has been initialized",
-  shortDesc: "",
-  key: "3",
-  codesample: "display.get_init()",
-},
-{
-  title: " display.set_mode(size, flags=0, depth=0) - Surface",
-  description: "Creates a display Surface and returns it",
-  shortDesc: "",
-  key: "4",
-  codesample: "display.set_mode((800, 600))",
-},
-{
-  title: " display.get_surface() - Surface",
-  description: "Returns the display Surface",
-  shortDesc: "",
-  key: "5",
-  codesample: "display.get_surface()",
-},
-{
-  title: " display.flip() - None",
-  description: "Updates portions of the screen",
-  shortDesc: "",
-  key: "6",
-  codesample: "display.flip()",
-},
-{
-  title: " display.update(rectangle=None) - None",
-  description: "Updates portions of the screen for software displays",
-  shortDesc: "",
-  key: "7",
-  codesample: "display.update()",
-},
-{
-  title: " display.Info() - VideoInfo",
-  description: "Create a video display information object",
-  shortDesc: "",
-  key: "8",
-  codesample: "display.Info()",
-},
-  ];
-const displayDescription = {
-title: "pygame display Description",
-description:
-  "pygame.display is a module that allows you to control the display window and screen",
-  };
-
-
-
 export type drawInfo = {
   title: string;
   description: string;
@@ -1983,7 +1909,6 @@ const pygameDescription = {
 
   
 export { 
-        displayData, displayDescription, 
         colorData, colorDescription, 
         drawData, drawDescription, 
         eventData, eventDescription ,
--- pages/display/index.tsx
+++ pages/display/index.tsx
@@ -1,19 +1,45 @@
-'use client'
+import React from "react";
+import { supabase } from './../../lib/supabaseClient';
+import DocLayout from "../../components/DocLayout";
 
-import { displayData, displayDescription } from "./../../documentation/docs"
-import DocLayout from '../../components/DocLayout';
+const DisplayDocsPage = ({ pygame_displayinfo_steps }) => {
+  const formattedSteps = pygame_displayinfo_steps
+    ? pygame_displayinfo_steps.map((step, index) => ({
+        key: index.toString(),
+        title: step.title,
+        description: step.description,
+        short_desc: step.short_desc,
+        codesample: step.code_sample || "",
+      }))
+    : [];
 
-const DisplayPage = () => {
+const displayDescription = {
+  title: "Pygame.display Module",
+  description:
+    "pygame.display is a module that allows you to control the display window and screen",
+};
 
   return (
-    <DocLayout 
-      title={displayDescription.title} 
-      baseDescription={displayDescription.description} 
-      data={displayData} 
-      description={displayDescription} 
+    <DocLayout
+      title={displayDescription.title}
+      baseDescription={pygame_displayinfo_steps?.description}
+      data={formattedSteps}
+      description={displayDescription}
     />
-
   );
 };
 
-export default DisplayPage;
+// get data from supabase
+export async function getServerSideProps() {
+  const { data: pygame_displayinfo_steps, error } = await supabase
+  .from('pygame_displayinfo_steps')
+  .select('*')
+
+  return {
+    props: {
+      pygame_displayinfo_steps,
+    },
+  };
+}
+
+export default DisplayDocsPage;
