f242b167b20405a17dbfbb1d37a853c4f8b31f37
huntermacias <supercodersf@gmail.com>
2023-04-17 03:38:06 -0700

    fixed text data

--- pages/terms-of-use/index.tsx
+++ pages/terms-of-use/index.tsx
@@ -1,49 +1,64 @@
-'use client'
+"use client";
+
+import { FaFileAlt } from "react-icons/fa";
 
 function TermsOfUse() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
-      <div className="max-w-4xl p-8 bg-gray-900 rounded-lg shadow-md font-mono">
-        <h1 className="text-2xl font-bold mb-8">Terms of Use</h1>
-        <p className="text-gray-100 mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum metus id
-          luctus tempus. Sed ultrices libero nec mauris rhoncus, eget pulvinar urna bibendum.
-        </p>
-        <h2 className="text-lg font-bold mb-4">1. Introduction</h2>
-        <p className="text-gray-100 mb-8">
-          Vestibulum commodo felis quis enim iaculis vulputate. Fusce euismod orci quis quam
-          condimentum, eget interdum purus bibendum. Duis sodales eu mauris in dapibus. Cras eu
-          arcu ullamcorper, faucibus ipsum eu, consequat nibh. Nullam gravida libero purus, id
-          commodo enim tristique sed.
-        </p>
-        <h2 className="text-lg font-bold mb-4">2. License to use the documentation</h2>
-        <p className="text-gray-100 mb-8">
-          Morbi euismod augue eget justo finibus efficitur. Fusce tempor magna eu est
-          facilisis, at ullamcorper mi aliquet. Nullam eget metus non nisi facilisis faucibus at
-          eu nibh. Sed vel enim ac eros ultrices lobortis. Aliquam aliquam viverra mi nec ornare.
-        </p>
-        <h2 className="text-lg font-bold mb-4">3. Restrictions</h2>
-        <p className="text-gray-100 mb-8">
-          Donec laoreet consequat sapien, nec bibendum metus placerat vel. Sed sit amet massa
-          eget sapien blandit suscipit sed a mauris. Curabitur euismod, mauris et efficitur
-          bibendum, ante nisl eleifend lacus, in fermentum risus odio ut justo.
-        </p>
-        <h2 className="text-lg font-bold mb-4">4. Termination</h2>
-        <p className="text-gray-100 mb-8">
-          Sed ac massa tristique, consectetur dolor eget, vestibulum eros. Morbi eget eros ut
-          velit bibendum ultricies ac eget elit. Fusce ac malesuada nulla. Sed ut massa in dolor
-          pharetra suscipit eu id odio. Quisque nec erat blandit, sagittis quam ut, lobortis
-          nibh.
-        </p>
-        <h2 className="text-lg font-bold mb-4">5. Governing Law</h2>
-        <p className="text-gray-100 mb-8">
-          Suspendisse quis est ut risus molestie lacinia. Vivamus vestibulum tristique velit,
-          vel commodo lectus vehicula vel. Suspendisse potenti. In hac habitasse platea dictumst.
-          Maecenas eleifend odio at sapien commodo varius.
-        </p>
+    <div className="bg-gray-900 min-h-screen mb-10">
+      <div className="max-w-4xl mx-auto px-4 py-12">
+        <div className="bg-white rounded-lg shadow-md font-mono text-gray-950 px-8 py-6">
+          <div className="flex items-center mb-6">
+            <FaFileAlt className="text-gray-400 mr-3" size={24} />
+            <h1 className="text-2xl font-bold">Terms of Use</h1>
+          </div>
+          <h2 className="text-lg font-bold mb-4">1. Introduction</h2>
+          <p className="text-black mb-8">
+            These Terms of Use (Terms) govern your access to and use of our
+            website and services (Service). By accessing or using the Service,
+            you agree to be bound by these Terms, which constitute a legally
+            binding agreement between you and us.
+          </p>
+          <h2 className="text-lg font-bold mb-4">
+            2. License to use the documentation
+          </h2>
+          <p className="text-black mb-8">
+            Subject to your compliance with these Terms, we grant you a limited,
+            non-exclusive, non-transferable license to access and use the
+            documentation provided with the Service solely for the purpose of
+            using the Service in accordance with these Terms.
+          </p>
+          <h2 className="text-lg font-bold mb-4">3. Restrictions</h2>
+          <p className="text-black mb-8">
+            You may not: (a) copy, modify, distribute, sell, or lease any part
+            of the Service or documentation; (b) reverse engineer or attempt to
+            extract the source code of the Service; (c) use the Service for any
+            unlawful purpose or in violation of any applicable law; (d)
+            interfere with or disrupt the integrity or performance of the
+            Service; (e) attempt to gain unauthorized access to the Service or
+            its related systems or networks; or (f) sublicense, transfer, or
+            assign any of the rights granted to you under these Terms.
+          </p>
+          <h2 className="text-lg font-bold mb-4">4. Termination</h2>
+          <p className="text-black mb-8">
+            We may terminate or suspend access to our Service immediately,
+            without prior notice or liability, for any reason whatsoever,
+            including without limitation if you breach these Terms. All
+            provisions of these Terms which by their nature should survive
+            termination shall survive termination, including, without
+            limitation, ownership provisions, warranty disclaimers, indemnity,
+            and limitations of liability.
+          </p>
+          <h2 className="text-lg font-bold mb-4">5. Governing Law</h2>
+          <p className="text-black mb-8">
+            These Terms and any dispute arising out of or related to these Terms
+            or the Service shall be governed by and construed in accordance with
+            the laws of the jurisdiction in which we are located, without giving
+            effect to any choice of law or conflict of law provisions.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 
-export default TermsOfUse
+export default TermsOfUse;
