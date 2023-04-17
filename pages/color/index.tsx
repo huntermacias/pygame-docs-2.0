9a6ee243426d29628be68f93ad5234cdb2ff13b1
huntermacias <supercodersf@gmail.com>
2023-04-16 22:52:02 -0700

    refactored data to supabase

--- pages/color/index.tsx
+++ pages/color/index.tsx
@@ -1,19 +1,45 @@
-'use client'
+import React from "react";
+import { supabase } from './../../lib/supabaseClient';
+import DocLayout from "../../components/DocLayout";
 
-import { colorData, colorDescription } from "./../../documentation/docs"
-import DocLayout from '../../components/DocLayout';
+const ColorPage = ({ pygame_colorinfo }) => {
+  const formattedSteps = pygame_colorinfo
+    ? pygame_colorinfo.map((step, index) => ({
+        key: index.toString(),
+        title: step.title,
+        description: step.description,
+        short_desc: step.short_desc,
+        codesample: step.code_sample || "",
+      }))
+    : [];
 
-const ColorPage = () => {
+    const colorDescription = {
+      title: "Pygame.color Module",
+      description:
+        "Welcome to the Pygame color module! pygame.Color is a module that provides various color-related functions and methods for working with colors in Pygame.",
+      };
 
   return (
-    <DocLayout 
-      title={colorDescription.title} 
-      baseDescription={colorDescription.description} 
-      data={colorData} 
-      description={colorDescription} 
+    <DocLayout
+      title={colorDescription.title}
+      baseDescription={pygame_colorinfo?.description}
+      data={formattedSteps}
+      description={colorDescription}
     />
-
   );
 };
+
+// get data from supabase
+export async function getServerSideProps() {
+  const { data: pygame_colorinfo, error } = await supabase
+  .from('pygame_colorinfo')
+  .select('*')
+
+  return {
+    props: {
+      pygame_colorinfo,
+    },
+  };
+}
 
 export default ColorPage;
--- pages/draw/index.tsx
+++ pages/draw/index.tsx
@@ -1,19 +1,44 @@
-'use client'
+import React from "react";
+import { supabase } from './../../lib/supabaseClient';
+import DocLayout from "../../components/DocLayout";
 
-import { drawData, drawDescription } from "./../../documentation/docs"
-import DocLayout from '../../components/DocLayout';
+const ColorPage = ({ pygame_drawinfo }) => {
+  const formattedSteps = pygame_drawinfo
+    ? pygame_drawinfo.map((step, index) => ({
+        key: index.toString(),
+        title: step.title,
+        description: step.description,
+        short_desc: step.short_desc,
+        codesample: step.code_sample || "",
+      }))
+    : [];
 
-const drawPage = () => {
+    const drawDescription = {
+      title: "pygame.draw Module",
+      description: "Welcome to the Pygame draw module!",
+      };
 
   return (
-    <DocLayout 
-      title={drawDescription.title} 
-      baseDescription={drawDescription.description} 
-      data={drawData} 
-      description={drawDescription} 
+    <DocLayout
+      title={drawDescription.title}
+      baseDescription={pygame_drawinfo?.description}
+      data={formattedSteps}
+      description={drawDescription}
     />
-
   );
 };
 
-export default drawPage;
+// get data from supabase
+export async function getServerSideProps() {
+  const { data: pygame_drawinfo, error } = await supabase
+  .from('pygame_drawinfo')
+  .select('*')
+
+  return {
+    props: {
+      pygame_drawinfo,
+    },
+  };
+}
+
+export default ColorPage;
