b6ba642e5586d8a7230fcc7a9053d624de244106
huntermacias <supercodersf@gmail.com>
2023-04-21 14:49:54 -0700

    🚚 set up supabase data

--- pages/mouse/index.tsx
+++ pages/mouse/index.tsx
@@ -1,19 +1,45 @@
-'use client'
+import React from "react";
+import { supabase } from './../../lib/supabaseClient';
+import DocLayout from "../../components/DocLayout";
 
-import { mouseData, mouseDescription } from "./../../documentation/docs"
-import DocLayout from '../../components/DocLayout';
+const MixerPage = ({ mouse_info }) => {
+  const formattedSteps = mouse_info
+    ? mouse_info.map((step, index) => ({
+        key: index.toString(),
+        title: step.title,
+        description: step.description,
+        shortDesc: step.shortDesc,
+        codesample: step.code_sample || "",
+      }))
+    : [];
 
-const MousePage = () => {
+const mouseDescription = {
+  title: "pygame.mixer Module",
+  description: "Pygame.mouse is a pygame module for loading and playing sounds. ",
+};
+
 
   return (
-    <DocLayout 
-      title={mouseDescription.title} 
-      baseDescription={mouseDescription.description} 
-      data={mouseData} 
-      description={mouseDescription} 
+    <DocLayout
+      title={mouseDescription.title}
+      baseDescription={mouse_info?.description}
+      data={formattedSteps}
+      description={mouseDescription}
     />
-
   );
 };
 
-export default MousePage;
+// get data from supabase
+export async function getServerSideProps() {
+  const { data: mouse_info, error } = await supabase
+  .from('mouse_info')
+  .select('*')
+
+  return {
+    props: {
+      mouse_info,
+    },
+  };
+}
+
+export default MixerPage;
